265469d4fe5b11541937822ef643115e783d5c7c
TseEn Wang <twang224@uic.edu>
2022-04-15 12:50:16 +0800

    Init Navbar

---
+++ e-commerce/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Navbar } from './components';
+import './App.css';
+const App = () => {
+  return (
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar/>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ e-commerce/src/components/navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className='navbar-links'>
+        <div className='navbar-links__logo'>
+        <img src="" alt="" />
+        </div>
+        <div className='navbar-links__container'>
+
+        </div>  
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
